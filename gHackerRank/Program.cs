5d5eefef8694c3d65dd01412129485a4a50a1fb3
GaryNg <garyng.zhongbo@gmail.com>
2016-01-02 22:52:46 +0800

    + Solve find-point

---
+++ gHackerRank/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solution
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int num = int.Parse(Console.ReadLine());
+			for (int i = 0; i < num; i++)
+			{
+				string input = Console.ReadLine();
+				FindPoint(input);
+			}
+
+			Console.ReadKey();
+		}
+
+		
+		static private void FindPoint(string input)
+		{
+			//Given two points P and Q, output the symmetric point of point P about Q.
+			List<int> points = input.Split(' ').ToList().Select(item => int.Parse(item)).ToList();
+			int x = points[2] * 2 - points[0];
+			int y = points[3] * 2 - points[1];
+			Console.WriteLine(x + " " + y);
+		}
+
+	}
+}
