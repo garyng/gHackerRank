e96e29423a2a2761eb18e6e89e5197f1f6fcc263
GaryNg <garyng.zhongbo@gmail.com>
2016-01-03 01:51:48 +0800

    + Solve minimum-draws

--- gHackerRank/Program.cs
+++ gHackerRank/Program.cs
@@ -13,20 +13,11 @@ namespace Solution
 			for (int i = 0; i < num; i++)
 			{
 				string input = Console.ReadLine();
-				FindPoint(input);
+				MinimumDraws(input);
+				
 			}
 
 			Console.ReadKey();
-		}
-
-		
-		static private void FindPoint(string input)
-		{
-			//Given two points P and Q, output the symmetric point of point P about Q.
-			List<int> points = input.Split(' ').ToList().Select(item => int.Parse(item)).ToList();
-			int x = points[2] * 2 - points[0];
-			int y = points[3] * 2 - points[1];
-			Console.WriteLine(x + " " + y);
 		}
 
 	}
---
+++ gHackerRank/hrSolutions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solution
+{
+	public static class hrSolutions
+	{
+		//Given two points P and Q, output the symmetric point of point P about Q.
+		static public void FindPoint(string input)
+		{
+			List<int> points = input.Split(' ').ToList().Select(item => int.Parse(item)).ToList();
+			int x = points[2] * 2 - points[0];
+			int y = points[3] * 2 - points[1];
+			Console.WriteLine(x + " " + y);
+		}
+
+		//Jim is off to a party and is searching for a matching pair of socks. 
+		//His drawer is filled with socks, each pair of a different color. 
+		//In its worst case scenario, how many socks (x) should Jim remove from his drawer until he finds a matching pair?
+		static public void MinimumDraws(string input)
+		{
+			int num = int.Parse(input);
+			Console.WriteLine(num + 1);
+		}
+
+	}
+}
